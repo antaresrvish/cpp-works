cb844554f2b9f4bbbc8660907c691bc5be3caf3a
Antares <67341887+antaresrvish@users.noreply.github.com>
2024-01-02 02:05:58 +0300

    apps

---
+++ apps/ogrenciort/ogr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;;
+
+int main()
+{
+    int stu_sessions = 0;
+    int stu_session_times = 0;
+
+    cout << "student session nums : ";
+    cin >> stu_sessions;
+    cout << "studen session times : ";
+    cin >> stu_session_times;
+    cout << stu_session_times + stu_sessions; //test
+
+    for (int i = 0; i < stu_sessions; i++)
+    {
+        
+    }
+
+}
